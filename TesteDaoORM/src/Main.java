eca960a1051874a1f7153e27abe86bfbf405e503
vinicius <viniciusarashiro0@gmail.com>
2021-10-12 18:25:17 -0300

    Adicionando operações de CRUD da classe ContaComumDAO

--- TesteDaoORM/src/Main.java
+++ TesteDaoORM/src/Main.java
@@ -1,3 +1,4 @@
+import modelo.ContaComum;
 import modelo.Pessoa;
 import modelo.PessoaFisica;
 import modelo.repositorio.*;
@@ -7,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		PessoaFisica p1 = new PessoaFisica();
+		/* PessoaFisica p1 = new PessoaFisica();
 		
 		p1.setNome("Joao Pereira");
 		p1.setEndereco("Rua Abc Def, 250, Sao Paulo - SP");
@@ -23,7 +24,39 @@ public class Main {
 		
 		System.out.println("ID da pessoa física inserida: " + p1.getId());
 		
-		pfDAO.fecharConexao();
+		pfDAO.fecharConexao(); */
+		
+		/* Criar conta comum */
+		
+		/* ContaComum c1 = ContaComum.abrirConta();
+		c1.setFechamento(new Date(2022 - 1900, 3, 25));
+		c1.setSaldo(4500.00);
+		c1.setSenha(123);
+		
+		ContaComumDAO c1DAO = new ContaComumDAO();
+		
+		long c1Numero = c1DAO.criarContaComum(c1);
+		
+		System.out.println("Número da conta comum criada: " + c1.getNumero()); 
+		 
+		c1DAO.fecharConexao(); */
+		
+		//ContaComumDAO c1DAO = new ContaComumDAO();
+		
+		//ContaComum numeroConta = c1DAO.recuperarContaComumPorNumero(4830522277178873107l);
+		//System.out.println("Número da conta: " + numeroConta.getNumero());
+		
+		//long numeroConta = c1DAO.removerContaComum(4830522277178873107l);
+		//System.out.println("Numero da conta removida: " + numeroConta);
+		
+		//ArrayList<ContaComum> contasComuns = c1DAO.recuperarContasComuns();
+		//contasComuns.forEach(conta -> System.out.println(conta.getNumero()));
+		
+		//long numeroConta = c1DAO.editarContaComum(4830522277178873107l, (byte) 2, 456);
+		//System.out.println("Conta editada!");
+		
+		//c1DAO.fecharConexao();
+		
 	}
 
 }
--- TesteDaoORM/src/modelo/repositorio/ContaComumDAO.java
+++ TesteDaoORM/src/modelo/repositorio/ContaComumDAO.java
@@ -1,7 +1,169 @@
 package modelo.repositorio;
 
+import java.sql.*;
+import java.util.ArrayList;
+
+import modelo.ContaComum;
+
 public class ContaComumDAO extends FabricaConexao
 {
-
+	public long criarContaComum(ContaComum contaComum) {
+		
+		long numero = 0;
+		
+		try 
+		{
+			String stmt = "insert into contascomuns (numero, abertura, fechamento, situacao, "
+					+ "senha, saldo) values (?, ?, ?, ?, ?, ?) returning numero";
+			
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			
+			pStmt.setLong(1, contaComum.getNumero());
+			pStmt.setDate(2, new java.sql.Date(contaComum.getAbertura().getTime()));
+			pStmt.setDate(3, new java.sql.Date(contaComum.getFechamento().getTime()));
+			pStmt.setByte(4, contaComum.getSituacao());
+			pStmt.setInt(5, contaComum.getSenha());
+			pStmt.setDouble(6, contaComum.getSaldo());
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) 
+			{
+				numero = rs.getLong("numero");
+			}
+		}
+		catch(Exception e) 
+		{
+			System.out.println("Erro ao criar Conta Comum " + e.getMessage());
+		}
+		
+		
+		return numero;
+	}
 	
+	public ArrayList<ContaComum> recuperarContasComuns() {
+		ArrayList<ContaComum> contasComuns = null;
+		
+		try
+		{
+			String stmt = "select numero, abertura, fechamento, situacao, senha, saldo "
+					+ "from contascomuns";
+			
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			contasComuns = new ArrayList<ContaComum>();
+			
+			while(rs.next())
+			{
+				ContaComum contaComum = new ContaComum(rs.getLong("numero"), 
+						rs.getDate("abertura"));
+				contaComum.setFechamento(rs.getDate("fechamento"));
+				contaComum.setSituacao(rs.getByte("situacao"));
+				contaComum.setSenha(rs.getInt("senha"));
+				contaComum.setSaldo(rs.getDouble("saldo"));
+				
+				contasComuns.add(contaComum);
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Erro ao tentar recuperar contas comuns! " + e.getMessage());
+		}
+		
+		return contasComuns;
+	}
+
+	public ContaComum recuperarContaComumPorNumero(long numero)
+	{
+		ContaComum contaComum = null;
+		
+		try
+		{
+			
+			String stmt = "select numero, abertura, fechamento, situacao, senha, saldo "
+					+ "from contascomuns where numero = ?";
+			
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			pStmt.setLong(1, numero);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			
+			
+			if(rs.next())
+			{
+				contaComum = new ContaComum(rs.getLong("numero"), rs.getDate("abertura"));
+				
+				contaComum.setFechamento(rs.getDate("fechamento"));
+				contaComum.setSituacao(rs.getByte("situacao"));
+				contaComum.setSenha(rs.getInt("senha"));
+				contaComum.setSaldo(rs.getDouble("saldo"));
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Erro ao tentar recuperar conta comum por número! " 
+		+ e.getMessage());
+		}
+		
+		return contaComum;
+	}
+
+	public long removerContaComum(long numero) {
+		
+		long numeroConta = 0;
+		
+		try
+		{
+			String stmt = "delete from contascomuns where numero = ? returning numero";
+			
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			
+			pStmt.setLong(1, numero);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next())
+			{
+				numeroConta = rs.getLong("numero");
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Erro ao tentar remover conta! " + e.getMessage());
+		}
+		
+		return numeroConta;
+	}
+
+	public long editarContaComum(long numero, byte situacao, int senha) {
+		long numeroConta = 0;
+		
+		try
+		{
+			String stmt = "update contascomuns SET situacao = ?, senha = ? "
+					+ "where numero = ? returning numero";
+			
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			
+			pStmt.setByte(1, situacao);
+			pStmt.setInt(2, senha);
+			pStmt.setLong(3, numero);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next())
+			{
+				numeroConta = rs.getLong("numero");
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Erro ao editar Conta Comum! " + e.getMessage());
+		}
+		
+		return numeroConta;
+	}
 }
